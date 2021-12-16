28a1e1d13999eb5245595224743336e5985f6cb4
waelbenmustapha <[Your>
2021-12-16 22:30:30 +0100

    kk

---
+++ front/src/components/Notification_Item.js
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+function Notification_Item() {
+    return (
+        <div style={{borderTop:'1px solid ',padding:'15px'}}>
+
+        </div>
+    )
+}
+
+export default Notification_Item
