eaa78cc11903fe6b97678c53e322d8e295ed76ab
waelbenmustapha <[Your>
2022-02-21 14:05:13 +0100

    ff

---
+++ front/src/components/test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div style={{display:'flex',justifyContent:'space-around'}}>
+            <div style={{width:'100px',height:'100px',backgroundColor:'black'}}></div>
+            <div style={{width:'100px',height:'100px',backgroundColor:'black'}}></div>
+            <div style={{width:'100px',height:'100px',backgroundColor:'black'}}></div>
+            <div style={{width:'100px',height:'100px',backgroundColor:'black'}}></div>
+            <div style={{width:'100px',height:'100px',backgroundColor:'black'}}></div>
+
+        </div>
+    )
+}
+
+export default Test
